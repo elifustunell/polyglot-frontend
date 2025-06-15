66213bae1526f62249bccf4e4abb185c4e2be676
elifustunell <elifustunel@outlook.com>
2025-06-15 11:26:56 +0300

    Update API URL for production

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,19 +1,20 @@
 // constants/Config.ts
+
 export const CONFIG = {
 API_BASE_URL: __DEV__
-? 'http://192.168.1.104:3000/api'  // Development
-: 'https://https://polyglotpall.onrender.com/api', // Production - Render URL'inizi buraya
+? 'http://192.168.1.104:3000/api' // Geliştirme IP (yerel)
+    : 'https://polyglotpall.onrender.com/api', // Render URL
 
-ENVIRONMENT: __DEV__ ? 'development' : 'production',
+  ENVIRONMENT: __DEV__ ? 'development' : 'production',
 
-// Firebase config (mevcut config'inizi buraya taşıyın)
-FIREBASE_CONFIG: {
-
+  FIREBASE_CONFIG: {
     apiKey: "AIzaSyBIadjUYHNEFKpDKnTGHxKtLrOAC8ST0JA",
     authDomain: "polyglotpal-16f33.firebaseapp.com",
     projectId: "polyglotpal-16f33",
     storageBucket: "polyglotpal-16f33.appspot.com",
     messagingSenderId: "89747440954",
     appId: "1:89747440954:android:2ab8b6884f9ebd617a7c03"
-     };
+  }
 };
+
+console.log('🔗 API Base URL:', CONFIG.API_BASE_URL);
